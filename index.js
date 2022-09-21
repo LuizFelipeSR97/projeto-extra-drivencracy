40d86535d497aca9e6161e4d6ecfc8433d9f44ee
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-09-21 15:10:42 -0300

    get/post da rota poll finalizados

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import {MongoClient} from 'mongodb';
+import {Db, MongoClient} from 'mongodb';
 import joi from 'joi';
 import bcrypt from 'bcrypt';
 import {v4 as uuid} from 'uuid';
@@ -14,7 +14,64 @@ server.use(cors());
 server.use(express.json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+    db = mongoClient.db("drivencracy")
+});
+
+// Rota poll
+
+server.get("/poll", async (req,res) => {
+
+    try {
+
+        const polls = await db.collection("polls").find().toArray();
+        res.send(polls);
+
+    } catch (err) {
+
+        res.status(500).send(err.message)
+
+    }
+})
+
+server.post("/poll", async (req,res) => {
+
+    const pollToSubmit = req.body;
+
+    if (pollToSubmit.title===""){
+        res.sendStatus(422)
+    }
+    if (pollToSubmit.expireAt===""){
+        // Pegar a data de hoje e acrescentar 30 dias
+        // Fazer pollToSubmit===[...pollToSubmit, expireAt: "ESSA NOVA DATA"]
+    }
+
+    try {
+
+        db.collection("polls").insertOne(pollToSubmit);
+        res.status(201).send(pollToSubmit);
+
+    } catch(err) {
+
+        res.status(500).send(err.message)
+
+    }
+})
+
+// Rota Choice
+
+server.get("/poll/:id/choice", async (req,res) => {
+
+    try {
+
+    } catch(err) {
+        res.status(500).send(err.message)
+    }
+})
 
 server.listen(process.env.PORT, ()=>{
+
     console.log(`Server running on port ${5000}`)
+
 })
